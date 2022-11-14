34285cca204dc21688cc91d873fd0bf97b800b01
Taemmini <taemin0614@hufs.ac.kr>
2022-11-14 16:05:56 +0900

    second_commit

--- Assignment/202002813_이태민_과제#02_binaryTree.py
+++ Assignment/202002813_이태민_과제#02_binaryTree.py
@@ -1,68 +1,75 @@
 class ListNode:
-    def __init__(self, data):
+    def __init__(self, data, next):
         self.data = data
-        self.next = None
+        self.next = next
 
-class LinkedQueue:
+
+MAX_QSIZE = 10
+
+
+class CircularQueue:
     def __init__(self):
-        self.front = self.rear = None
-        
+        self.front = 0
+        self.rear = 0
+        self.items = [None] * MAX_QSIZE
+
     def isEmpty(self):
-        return self.front == None
-    
+        return self.front == self.rear
+
+    def isFull(self):
+        return self.front == (self.rear+1) % MAX_QSIZE
+
     def EnQueue(self, item):
-        temp = ListNode(item)
-        
-        if self.rear == None:
-            self.front = self.rear = temp
-            return
-        self.rear.next = temp
-        self.rear = temp
-        
+        if not self.isFull():
+            self.rear = (self.rear+1) % MAX_QSIZE
+            self.items[self.rear] = item
+
     def DeQueue(self):
         if not self.isEmpty():
-            temp = self.front
-            self.front = temp.next
-            return temp.data
+            self.front = (self.front+1) % MAX_QSIZE
+            return self.items[self.front]
 
-class TreeNode:								
-    def __init__ (self, item, left, right):	
-        self.data = item				
+
+class TreeNode:
+    def __init__(self, item, left, right):
+        self.data = item
         self.left = left
         self.right = right
-        
+
     def size(self):
         if self.left:
-            left = self.left.size
+            left = self.left.size()
         else:
             left = 0
-            
+
         if self.right:
             right = self.right.size()
         else:
             right = 0
         return left + right + 1
-    
+
     def depth(self):
         if self.left:
             left_depth = self.left.depth()
         else:
             left_depth = 0
-            
+
         if self.right:
             right_depth = self.right.depth()
         else:
             right_depth = 0
         return max(left_depth, right_depth) + 1
-    
+
     def leaf(self):
         if self.data is None:
             return 0
         elif self.left is None and self.right is None:
             return 1
         else:
-            return TreeNode.leaf(self.left) + TreeNode.leaf(self.rignt)
-    
+            left = self.left.size()
+            right = self.right.size()
+            return left + right
+
     def preorder(self):
         traversal = []
         traversal.append(self.data)
@@ -80,7 +87,7 @@ class TreeNode:
         if self.right:
             traversal += self.right.inorder()
         return traversal
-    
+
     def postorder(self):
         traversal = []
         if self.left:
@@ -89,59 +96,79 @@ class TreeNode:
             traversal += self.right.inorder()
         traversal.append(self.data)
         return traversal
-    
+
     def levelorder(self):
-        queue = LinkedQueue()
+        queue = CircularQueue()
         queue.EnQueue(self.data)
         while not queue.isEmpty():
-            n = queue.Dequeue()
+            n = queue.DeQueue()
             if n is not None:
                 print(n.data, end=' ')
                 queue.EnQueue(n.left)
                 queue.EnQueue(n.right)
 
+
 class BinaryTree:
     def __init__(self, root):
         self.root = root
-    
+
     def size(self):
         if self.root:
             return self.root.size()
         else:
             return 0
-    
+
     def depth(self):
         if self.root:
             return self.root.depth()
         else:
             return 0
-    
+
     def leaf(self):
         if self.root:
             return self.root.leaf()
         else:
             return 0
-    
+
     def preorder(self):
         if self.root:
-            return self.root.prrorder()
+            return self.root.preorder()
         else:
             return []
-        
+
     def inorder(self):
         if self.root:
             return self.root.inorder()
         else:
             return []
-    
+
     def postorder(self):
         if self.root:
             return self.root.postorder()
         else:
             return []
-    
+
     def levelorder(self):
         if self.root:
             return self.root.levelorder()
         else:
             return []
+
+
+# Test
+d = TreeNode('D', None, None)
+e = TreeNode('E', None, None)
+b = TreeNode('B', d, e)
+f = TreeNode('F', None, None)
+c = TreeNode('C', f, None)
+root = TreeNode('A', b, c)
+Tree = BinaryTree(root)
+
+print(f' In-Order : {Tree.inorder()}')
+print(f'Pre-Order : {Tree.preorder()}')
+print(f'Post-Order : {Tree.postorder()}')
+print(f'levelorder : {Tree.levelorder()}')
+
+print(f'노드의 개수 = {Tree.size()}개')
+print(f'트리의 높이 = {Tree.depth()}')
+print(f'단말의 개수 = {Tree.leaf()}개')
